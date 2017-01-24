158bbb206b32be0b8d3b2ca7d990477b0a73c396
vagrant <ragesh.1995@gmail.com>
2017-01-24 05:08:06 +0000

    sum refactored

--- js/array/sum.js
+++ js/array/sum.js
@@ -1,10 +1,10 @@
 module.exports = (x)=>{
-	var map=new Map();
-	for(var i=0;i<x.length;i++){
-		map[i+1]=0;
-		for(var j=0;j<x[i].length;j++){
-			map[i+1]+=x[i][j];
+	let map = new Map();
+	for(let i = 0; i < x.length; i = i + 1) {
+		map[i + 1] = 0;
+		for(let j = 0; j < x[i].length; j = j + 1) {
+			map[i + 1] = map[i + 1] + x[i][j];
 		}
 	}
 	return map;
-}
+};
--- js/testCodeQuality.js
+++ js/testCodeQuality.js
@@ -1,39 +1,40 @@
-const fs=require('fs');
-var readLine=require('readline');
-var barArr=[];
-var lineArr=[];
-for(var i=2001;i<=2016;i++){
-  var bobj={year: i, OVER$500: 0, $500ANDUNDER: 0};
-  var lobj={year: i, ARRESTED: 0, NOTARRESTED: 0};
+const fs = require('fs');
+const readLine = require('readline');
+let barArr = [];
+let lineArr = [];
+for(let i = 2001;i <= 2016;i += 1){
+  let bobj = {year: i, OVER$500: 0, $500ANDUNDER: 0};
+  let lobj = {year: i, ARRESTED: 0, NOTARRESTED: 0};
   barArr.push(bobj);
   lineArr.push(lobj);
 }
-var rd=readLine.createInterface({
+let rd = readLine.createInterface({
   input: fs.createReadStream('inputdata/crimedata.csv'),
   output: process.stdout,
   terminal: false
 });
 rd.on('line',function(line){
-  var data=line.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/);
-  var id=data[0]+"";
-  if(id.length<=4)
+  let data = line.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/);
+  let id = data[0] + "";
+  if(id.length <= 4){
    return;
-  var year=data[17];
-  var arrested=data[8];
-  var type=data[6];
-  if(data[5]==="ASSAULT"){
-    if(arrested==="TRUE"||arrested==="true"){
-      lineArr[year-2001].ARRESTED++;
+  }
+  let year = data[17];
+  let arrested = data[8];
+  let type = data[6];
+  if(data[5] === "ASSAULT"){
+    if(arrested === "TRUE" || arrested === "true"){
+      lineArr[year - 2001].ARRESTED += 1;
     }
-    else if(arrested==="FALSE"||arrested==="false"){
-      lineArr[year-2001].NOTARRESTED++;
+    else if(arrested === "FALSE" || arrested === "false"){
+      lineArr[year - 2001].NOTARRESTED += 1;
     }
   }
-  if(type==="OVER $500"){
-    barArr[year-2001].OVER$500++;
+  if(type === "OVER $500"){
+    barArr[year - 2001].OVER$500 += 1;
   }
-  else if(type==="$500 AND UNDER"){
-    barArr[year-2001].$500ANDUNDER++;
+  else if(type === "$500 AND UNDER"){
+    barArr[year - 2001].$500ANDUNDER += 1;
   }
 });
 rd.on('close',function(){
